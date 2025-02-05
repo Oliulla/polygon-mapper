950a7663a75e2c894e9fbae96d62da60e49c3395
Oliulla <mohammad.oliullah25@gmail.com>
2025-02-06 02:41:21 +0600

    Import sidebar in dynamic way as client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Sidebar from "@/components/Sidebar";
 import dynamic from "next/dynamic";
 
 const Map = dynamic(() => import("@/components/Map"), { ssr: false });
+const Sidebar = dynamic(() => import("@/components/Sidebar"), { ssr: false });
 
 export default function Home() {
   return (
